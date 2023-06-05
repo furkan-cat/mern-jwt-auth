cf13a8f6797b189aca56fe3b4dcf988ccc813fef
Furkan Çat <furi544@gmail.com>
2023-06-06 01:07:57 +0300

    multiple exports

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as FormContainer } from "./FormContainer";
+export { default as Hero } from "./Hero";
+export { default as Header } from "./Header";
+export { default as Loader } from "./Loader";
