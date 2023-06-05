33f31b9fe20f49fdaa5cbd838d57c18bb2148d20
Furkan Çat <furi544@gmail.com>
2023-06-06 01:09:35 +0300

    api requests register, login, logout

---
+++ frontend/src/libs/store/slices/apiSlice.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "apiSlice",
+  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "api/users",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "api/users/auth",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "api/users/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation, useLogoutMutation } =
+  apiSlice;
