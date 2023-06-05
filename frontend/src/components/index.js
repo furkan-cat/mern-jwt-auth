71f9f1aecfe13ff608474a47d9c6e299dda95e91
Furkan Çat <furi544@gmail.com>
2023-06-06 02:02:00 +0300

    private route

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export { default as FormContainer } from "./FormContainer";
 export { default as Hero } from "./Hero";
 export { default as Header } from "./Header";
 export { default as Loader } from "./Loader";
+export { default as PrivateRoute } from "./PrivateRoute";
