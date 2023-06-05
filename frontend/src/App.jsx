1d830c7c9a9c59d946b4fd1f417d4614e86ff96c
Furkan Çat <furi544@gmail.com>
2023-06-06 01:07:17 +0300

    app and main js comps, integrated react router structure, redux store

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <ToastContainer />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { Login, Home, Register } from "./pages/index";
+import store from "./libs/store/store.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
